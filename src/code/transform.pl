bda5d2e535664ade02c5062ac6a2b6897fbc189c
Peter Flach <Peter.Flach@bristol.ac.uk>
2021-12-24 09:04:17 +0000

    Update transform.pl

--- src/code/transform.pl
+++ src/code/transform.pl
@@ -6,3 +6,11 @@ transform(Formula,Clauses):-
    conjunctive_normal_form(F4,F5),
    clausal_form(F5,Clauses).
 
+/** <examples>
+?- transform( forall(Y,exists(X,mother_of(X,Y))) &
+             -forall(Z,exists(W,woman(Z) => mother_of(Z,W))),
+              CL).
+?- transform( forall(X,exists(Y,mouse(X) => tail_of(Y,X))),CL).
+?- transform( forall(X,exists(Y,loves(X,Y)) & forall(Z,loves(Y,Z))),CL).
+?- transform( forall(X,forall(Y,exists(Z,number(X) & number(Y) => maximum(Z,X,Y)))),CL).
+*/
